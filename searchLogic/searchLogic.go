f97bb9cf531758e2c9b004b1829dbaa0bce2d48b
jakedevar <devarennesjacob@gmail.com>
2023-04-14 11:00:50 -0500

    updated cors

--- searchLogic/searchLogic.go
+++ searchLogic/searchLogic.go
@@ -112,7 +112,7 @@ func HandleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
       w.Write([]byte("encoding failure"))
       return
     }
-    // enableCORS(&w)
+    enableCORS(&w)
     w.Header().Set("Content-Type", "application/json")
     w.Write(buf.Bytes())
   }
@@ -266,8 +266,8 @@ func createSearchResultsArray(s *Searcher, idxs [][]int, searchTerm string) Sear
   return results
 }
 
-// func enableCORS(w *http.ResponseWriter) {
-//   (*w).Header().Set("Access-Control-Allow-Origin", "*")
-//   (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-//   (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-// }
+func enableCORS(w *http.ResponseWriter) {
+  (*w).Header().Set("Access-Control-Allow-Origin", "*")
+  (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+  (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
